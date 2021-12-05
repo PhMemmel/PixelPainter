4208ae05e5fc56d5ce0964abeb066f926e5e89e6
Philipp Memmel <philipp.memmel@thg.muenchen.musin.de>
2021-12-05 10:56:07 +0100

    Reacting to events, making observer functional, painting works

--- src/main/java/edv/memmel/pixelpainter/view/GameWindowFxController.java
+++ src/main/java/edv/memmel/pixelpainter/view/GameWindowFxController.java
@@ -1,25 +1,34 @@
 package edv.memmel.pixelpainter.view;
 
+import edv.memmel.pixelpainter.model.Coordinate;
 import edv.memmel.pixelpainter.model.PixelManager;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 /** FX controller class for main window. */
 public class GameWindowFxController {
 
   private final int GRID_SIZE = 20;
+  private final String labelCss = "-fx-border-color: \"black\"";
 
   @FXML GridPane grid;
   @FXML Label currentColorLabel;
+  @FXML ColorPicker colorPicker;
 
   private Label[][] labels;
+  private PixelManager pixelManager;
   private ModelObserver modelObserver;
 
   public GameWindowFxController() {
+    pixelManager = new PixelManager(GRID_SIZE);
     labels = new Label[GRID_SIZE][GRID_SIZE];
-    modelObserver = new ModelObserver(new PixelManager(GRID_SIZE));
+    modelObserver = new ModelObserver(pixelManager);
   }
 
   @FXML
@@ -27,15 +36,43 @@ public class GameWindowFxController {
     for (int i = 0; i < GRID_SIZE; i++) {
       for (int j = 0; j < GRID_SIZE; j++) {
         Label label = new Label("");
-        label.setStyle("-fx-background-color: \"lightgrey\"; -fx-border-color: \"black\"");
+        label.setStyle(labelCss);
         label.setMinWidth(20.0);
         label.setMinHeight(20.0);
         label.setAlignment(Pos.CENTER);
-        grid.add(label, i, j);
+        label.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> setColor(event));
+        grid.add(label, j, i); // care: first column, then row
         labels[i][j] = label;
       }
     }
+    // initialize labels in the model
+    resetPixels();
+    // Set neccessary references:
     modelObserver.setGuiPixels(labels);
     modelObserver.setCurrentColorLabel(currentColorLabel);
+    modelObserver.setColorPicker(colorPicker);
+  }
+
+  private void setColor(MouseEvent event) {
+    Coordinate coordinate =
+        new Coordinate(
+            GridPane.getRowIndex((Node) event.getTarget()),
+            GridPane.getColumnIndex((Node) event.getTarget()));
+    pixelManager.colorizePixel(coordinate);
+  }
+
+  @FXML
+  void changeColor() {
+    pixelManager.setCurrentPaintColor(colorPicker.getValue().toString());
+  }
+
+  @FXML
+  void resetPixels() {
+    pixelManager.setCurrentPaintColor("lightgrey");
+    for (int i = 0; i < GRID_SIZE; i++) {
+      for (int j = 0; j < GRID_SIZE; j++) {
+        pixelManager.colorizePixel(new Coordinate(i, j));
+      }
+    }
   }
 }
--- src/main/java/edv/memmel/pixelpainter/view/ModelObserver.java
+++ src/main/java/edv/memmel/pixelpainter/view/ModelObserver.java
@@ -1,41 +1,63 @@
 package edv.memmel.pixelpainter.view;
 
+import edv.memmel.pixelpainter.model.Coordinate;
 import edv.memmel.pixelpainter.model.PixelManager;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 class ModelObserver implements PropertyChangeListener {
-
 
   private Label[][] guiPixels;
   private Label currentColorLabel;
   private PixelManager pixelManager;
+  private ColorPicker colorPicker;
 
   ModelObserver(PixelManager pixelManager) {
     this.pixelManager = pixelManager;
+    pixelManager.addPropertyChangeListener(this);
   }
 
   @Override
   public void propertyChange(PropertyChangeEvent evt) {
     switch (evt.getPropertyName()) {
       case "pixelColorChanged":
-
+        Coordinate coordinate = (Coordinate) evt.getNewValue();
+        Color color = Color.valueOf(pixelManager.getPixelColorAt(coordinate));
+        Platform.runLater(
+            () ->
+                guiPixels[coordinate.getX()][coordinate.getY()].setBackground(
+                    new Background(new BackgroundFill(color, new CornerRadii(0), Insets.EMPTY))));
         break;
       case "pixelDisabledStatusChanged":
-
+        // TODO
         break;
       case "currentPaintColorChanged":
-
+        Platform.runLater(
+            () -> {
+              currentColorLabel.setText(pixelManager.getCurrentPaintColor());
+              colorPicker.setValue(Color.valueOf(pixelManager.getCurrentPaintColor()));
+            });
         break;
     }
   }
 
-  public void setCurrentColorLabel(Label currentColorLabel) {
+  void setCurrentColorLabel(Label currentColorLabel) {
     this.currentColorLabel = currentColorLabel;
   }
 
-  public void setGuiPixels(Label[][] guiPixels) {
+  void setColorPicker(ColorPicker colorPicker) {
+    this.colorPicker = colorPicker;
+  }
+
+  void setGuiPixels(Label[][] guiPixels) {
     this.guiPixels = guiPixels;
   }
 }
